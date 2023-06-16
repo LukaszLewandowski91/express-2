9d69e8fbb5e47a924b8d2eb5bb8d3cb519a94095
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-06-16 16:18:38 +0200

    add contact form and multer

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("express-handlebars");
+const multer = require("multer");
+const upload = multer({ dest: "public/" });
 const app = express();
 
 app.engine(".hbs", hbs());
 app.set("view engine", ".hbs");
 
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -30,6 +33,15 @@ app.get("/history", (req, res) => {
 
 app.get("/hello/:name", (req, res) => {
   res.render("hello", { name: req.params.name });
+});
+
+app.post("/contact/send-message", upload.single("myImage"), (req, res) => {
+  const { author, sender, title, message } = req.body;
+  if (author && sender && title && message && req.file) {
+    res.render("contact", { isSent: true, myImage: req.file.originalname });
+  } else {
+    res.render("contact", { isError: true });
+  }
 });
 
 app.use((req, res) => {
